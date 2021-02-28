94dee1243b303a7dd02ae93db109fc1e300df431
Richard Joel <richard_joyal@outlook.com>
2021-02-28 15:41:35 +0530

    CSS problem fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./components/login";
 import Test from "./components/test";
 import Upload from "./components/upload";
 import TestEnd from "./components/test_end";
-import FacultyTest from "./components/faculty_test";
+//import FacultyTest from "./components/faculty_test";
 import TestList from "./components/test_list";
 import Profile from "./components/profile";
 import "./App.css";
@@ -23,7 +23,7 @@ class App extends Component {
           <Route path="/login" component={Login}></Route>
           <Route path="/upload" component={Upload}></Route>
           <Route path="/end" exact to component={TestEnd}></Route>
-          <Route path="/test_faculty" component={FacultyTest}></Route>
+          {/* <Route path="/test_faculty" component={FacultyTest}></Route> */}
           <Route path="/tests" to component={TestList}></Route>
           <Route path="/profile" to component={Profile}></Route>
         </Switch>

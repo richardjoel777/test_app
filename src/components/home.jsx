a79c7096d99bc51e10d78d392648423285b8215f
OviYaD <69393959+OviYaD@users.noreply.github.com>
2021-04-16 10:08:20 +0530

    update

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { getTests } from "../api/questions";
-import bg from './stu 1.png';
+import bg from './stu 1.jpeg';
 import style from './test_code.module.css'
 const Home = () => {
+ 
   return (
-
     <div className="test_end">
     <img src={bg} alt="bg" ></img>
     <div className={style.blog}>
         <p className={style.disc}>Enter The Test Code!</p>
-        <input type="text" className={style.input}></input>
+        <form><input type="text" className={style.input}></input>
         <button type="submit" className={style.btn}>GO!</button>
-        <button type="submit" className={style.btn} onClick={() => localStorage.removeItem("TestAppUser")}>LOG OUT</button>
+        <button type="submit" className={style.btn} onClick={() => localStorage.removeItem("TestAppUser")}>LOG OUT</button></form>
+        
+        
     </div>
 </div>
   );

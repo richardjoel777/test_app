397cfc1554ce3689d64fad373fd82e76148f1b82
priyadharshini149 <69582723+priyadharshini149@users.noreply.github.com>
2021-04-16 20:38:35 +0530

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,30 +1,34 @@
-import React from "react";
-import { Dropdown } from "react-bootstrap";
-import "./test_table.css";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {ListGroup} from 'react-bootstrap';
+import './test_table.css';
 
 function List() {
-  return (
-    <div className="App-head">
-      <Dropdown>
-        <Dropdown.Toggle
-          variant="primary"
-          style={{
-            backgroundColor: "rgb(185, 159, 223)",
-            color: "black",
-          }}
-          id="dropdown-basic"
-        >
-          Class and Sec
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">CSE-A</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">CSE-B</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">CSE-C</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">CSE-D</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </div>
-  );
+    return (
+     
+        < div className="A-h">
+  
+  
+<ListGroup >
+<ListGroup.Item style={{backgroundColor:"rgb(136, 119, 163)"}} >CLASS & SEC</ListGroup.Item>
+  <ListGroup.Item style={{backgroundColor:"rgb(185, 159, 223)"}} >CSE-A</ListGroup.Item>
+  <ListGroup.Item style={{backgroundColor:"rgb(185, 159, 223)"}} >CSE-B</ListGroup.Item>
+  <ListGroup.Item style={{backgroundColor:"rgb(185, 159, 223)"}} >CSE-C</ListGroup.Item>
+  <ListGroup.Item style={{backgroundColor:"rgb(185, 159, 223)"}} >CSE-D</ListGroup.Item>
+  
+  
+</ListGroup>
+
+
+</div>
+    );
+    
+
 }
+
+
+
+
+
 
 export default List;

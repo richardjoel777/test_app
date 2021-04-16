82f47668212fdc63c30b4b417d3a3bf8bd425914
OviYaD <69393959+OviYaD@users.noreply.github.com>
2021-04-16 16:24:47 +0530

    styles fixed

--- src/components/test_confirm.jsx
+++ src/components/test_confirm.jsx
@@ -1,37 +1,37 @@
 import React from "react";
 import "./test_confirm.css";
-const TestConfirm = ({ onClick, course, test_length, student }) => {
+const TestConfirm = ({ onClick, course, data_length, student }) => {
   return (
     <div>
       <center>
-        <h1>
-          <b>KONGU ENGINEERING COLLEGE </b>
+        <h1 style={{fontFamily:'"Poppins", sans-serif',fontWeight:500,fontSize:50}}>
+          KONGU ENGINEERING COLLEGE
         </h1>
       </center>
       <div className="container-confirm">
-        <h2>
-          {student.name} , {student.id}
+        <h2 style={{fontFamily:'"Poppins", sans-serif',fontWeight:600}}>
+          {student.name} <br/> {student.roll}
         </h2>
         <div className="card">
           <div className="card-body">
-            <p>
-              <b>COURSE CODE: {course.code}</b>
+            <p style={{fontFamily:'"Poppins", sans-serif',fontWeight:500,fontSize:30}}>
+             COURSE CODE: {course.code}
             </p>
-            <p>
-              <b>COURSE NAME: {course.title}</b>
+            <p style={{fontFamily:'"Poppins", sans-serif',fontWeight:500,fontSize:30}}>
+              COURSE NAME: {course.title}
             </p>
-            <p>
-              <b>TOTAL QUESTION: {test_length}</b>
+            <p style={{fontFamily:'"Poppins", sans-serif',fontWeight:500,fontSize:30}}>
+              TOTAL QUESTION: {data_length}
             </p>
-            <p>
-              <b>TIMING SCHEDULE</b>
+            <p style={{fontFamily:'"Poppins", sans-serif',fontWeight:500,fontSize:30}}>
+              TIMING SCHEDULE
               <br />
               Easy: 60 Secs
               <br />
               Hard: 120 Secs
             </p>
             <button className="button" onClick={onClick}>
-              <b>TAKE TEST</b>
+              <b style={{fontFamily:'"Poppins", sans-serif',fontWeight:500,fontSize:30}}>TAKE TEST</b>
             </button>
           </div>
         </div>

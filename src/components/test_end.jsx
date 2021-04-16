ab1dccb1542a09a757c0af3eb8c4264fa89fa785
priyadharshini149 <69582723+priyadharshini149@users.noreply.github.com>
2021-04-16 19:20:33 +0530

    Update test_end.jsx

--- src/components/test_end.jsx
+++ src/components/test_end.jsx
@@ -1,10 +1,12 @@
 import React,{Component} from "react";
 import styles from "./test_end.module.css";
+import ParticlesBg from "particles-bg";
 class TestEnd extends Component {
  
   render(){
     return (
       <div className="test_end">
+     <ParticlesBg type="polygon"  bg={true} />
       <p className={styles.head}>KONGU ENGINEERING COLLEGE</p>
         <div className={styles.disp}>
           <h1 className={styles.discription}>Test is Successfully Submitted!</h1><br/>
